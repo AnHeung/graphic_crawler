fecf97adab16111ece546b40ab12d19ca984e8a4
gskebin <gskebin4@gmail.com>
2020-11-11 14:20:41 +0900

    루리웹 모바일 주소로 다시 변경

--- ruriweb_crawler.js
+++ ruriweb_crawler.js
@@ -1,13 +1,13 @@
 const { sendSlackMsg } = require('./slack');
 const { filterWord } = require('./util');
 
-exports.run = async () => {
+(async () => {
 
-    const ruliwebUrl = 'https://bbs.ruliweb.com/market/board/1020'
+    const ruliwebUrl = 'https://m.ruliweb.com/market/board/1020'
     
-    const baseUrl = "https://bbs.ruliweb.com/"
-
     const cheerio = require('cheerio');
+
+    const baseUrl = 'https://m.ruliweb.com/'
 
     const axios = require('axios');
 
@@ -17,12 +17,12 @@ exports.run = async () => {
 
     const $ = cheerio.load(site.data)
 
-    const result = Array.from($('tr.table_body'))
+    const result = Array.from($('td.subject'))
         .map(data => {
             const title = $(data).find('a.deco').text().replace(/([\t|\n|\s])/gi, "")
             const url =  $(data).find('a.deco').attr('href')
             const category = "루리웹"
-            const date = $(data).find('td.time').text().replace(/([\t|\n|\s])/gi, "")
+            const date = $(data).find('span.time').text().replace(/([\t|\n|\s])/gi, "")
             return { category, title, url, date }
         })
         .filter(({title})=>{
@@ -31,5 +31,6 @@ exports.run = async () => {
         
     if (result.length > 0) await sendSlackMsg(result)
 
-}
+})()
+
 
