4fb51d1faeeb9ade10ba1c0b41150bb7eeb85f69
gskebin <gskebin4@gmail.com>
2020-11-19 16:33:20 +0900

    서버 부분 삭제(scheduler 가 해줄거임)

--- api.js
+++ api.js
@@ -1,28 +1,14 @@
-const schedule = require('node-schedule')
 const quasarzone_crawler = require('./quasarzone_crawler')
 const ruriweb_crawler = require('./ruliweb_crawler')
-const express = require('express')
-const app = express()
-const port = 5500
 
-async function run_crawler() {
+exports.run = ()=>{
     try {
-        console.log('*** run_crawler start ***')
+        console.log('*** graphic_card_crawler start ***')
         await quasarzone_crawler.run()
         await ruriweb_crawler.run()
-        console.log('*** run_crawler end ***')
+        console.log('*** graphic_card_crawler end ***')
     } catch (error) {
         console.error(error)
         process.exit()
     } 
 }
-
-schedule.scheduleJob('*/1 * * * *', run_crawler)
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
